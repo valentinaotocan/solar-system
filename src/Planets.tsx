701bec715b477f2f04a7276184ff7ca99787a88d
Valentina Otočan <valentinaotocan@gmail.com>
2023-09-16 20:46:19 +0200

    Add content for planet__information div

--- src/Planets.tsx
+++ src/Planets.tsx
@@ -13,16 +13,38 @@ function Planets() {
   }
 
   return (
-    <section>
+    <section className="planet">
       <OverviewImage />
       <div>
         <h1>{planet.name}</h1>
         <OverviewText planet={planet} />
         <p>
-          <a href={planet.overview.source} target="_blank" rel="noopener noreferrer">
+          <a
+            href={planet.overview.source}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Source : Wikipedia
           </a>
         </p>
+      </div>
+      <div className="planet__information">
+        <div>
+          <p>Rotation time</p>
+          <h2>{planet.rotation}</h2>
+        </div>
+        <div>
+          <p>Revolution time</p>
+          <h2>{planet.revolution}</h2>
+        </div>
+        <div>
+          <p>Radius time</p>
+          <h2>{planet.radius}</h2>
+        </div>
+        <div>
+          <p>Average temp.</p>
+          <h2>{planet.temperature}</h2>
+        </div>
       </div>
     </section>
   );
