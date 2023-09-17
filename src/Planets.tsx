67772ad04a0c1480a76eb6757940cea210525781
Valentina Otočan <valentinaotocan@gmail.com>
2023-09-17 11:40:17 +0200

    Style for planet__information__div

--- src/Planets.tsx
+++ src/Planets.tsx
@@ -14,37 +14,41 @@ function Planets() {
 
   return (
     <section className="planet">
-      <OverviewImage />
-      <div>
-        <h1>{planet.name}</h1>
-        <OverviewText planet={planet} />
-        <p>
-          <a
-            href={planet.overview.source}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Source : Wikipedia
-          </a>
-        </p>
-        <button>Overview</button>
-        <button>Internal structure</button>
-        <button>Surface geology</button>
+      <div className="planet__main">
+        <div className="planet__main__image">
+          <OverviewImage />
+        </div>
+        <div className="planet__main__text">
+          <h1>{planet.name}</h1>
+          <OverviewText planet={planet} />
+          <p>
+            <a
+              href={planet.overview.source}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Source : Wikipedia
+            </a>
+          </p>
+          <button>Overview</button>
+          <button>Internal structure</button>
+          <button>Surface geology</button>
+        </div>
       </div>
       <div className="planet__information">
-        <div>
+        <div className="planet__information__div">
           <p>Rotation time</p>
           <h2>{planet.rotation}</h2>
         </div>
-        <div>
+        <div className="planet__information__div">
           <p>Revolution time</p>
           <h2>{planet.revolution}</h2>
         </div>
-        <div>
+        <div className="planet__information__div">
           <p>Radius time</p>
           <h2>{planet.radius}</h2>
         </div>
-        <div>
+        <div className="planet__information__div">
           <p>Average temp.</p>
           <h2>{planet.temperature}</h2>
         </div>
